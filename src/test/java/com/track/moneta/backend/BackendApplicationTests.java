48f9084006326754a2d6907f060725371d0e04b2
Aditya <adityasangeetha@gmail.com>
2025-11-13 21:20:51 +0530

    Build successful

--- src/test/java/com/track/moneta/backend/BackendApplicationTests.java
+++ src/test/java/com/track/moneta/backend/BackendApplicationTests.java
@@ -1,13 +1,23 @@
 package com.track.moneta.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void init() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
